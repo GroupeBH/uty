6acc69b4ab0ef98bd452992f806b9ea12bf13387
eugene bosuku <bosukuege@gmail.com>
2026-03-25 04:30:14 +0100

    api url to uri on env

--- app.config.ts
+++ app.config.ts
@@ -141,7 +141,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         },
         android: {
             package: 'com.uty',
-            versionCode: 34,
+            versionCode: 35,
             softwareKeyboardLayoutMode: 'resize',
             permissions: ['com.google.android.gms.permission.AD_ID'],
             ...(hasFirebaseApp ? { googleServicesFile: androidGoogleServicesFile } : {}),
